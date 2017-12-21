dbfff5c052134fcc4599b628fac6f34561aab30c
Antoine Muller <nemak121@gmail.com>
2017-12-21 15:17:56 +0100

    Add AddTodoForm

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,12 @@
-import * as React from 'react';
+import * as React from 'react'
+import AddTodo from '../containers/AddTodo'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <h1>Todos</h1>
+        <AddTodo />
       </div>
     );
   }
